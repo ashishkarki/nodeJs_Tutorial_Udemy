80d3ce424174151c570cb2ce6bbae4edfb816131
Ashish Karki <akarki@mimecast.com>
2018-05-01 19:05:56 -0400

    Adding more greeting stuff

--- app.js
+++ app.js
@@ -11,3 +11,16 @@ greet2();
 
 var greet3 = require('./greet3');
 greet3.greet();
+greet3.greeting = "Updated greeting which is used everywhere from now on for greet3.";
+
+var greet3b = require('./greet3');
+greet3b.greet(); // this will output the changed greeting
+
+var Greet4 = require('./greet4');
+var gtr4 = new Greet4();
+gtr4.greet();
+
+var greet5 = require('./greet5').greet;
+greet5();
+
+const util = require('util');
