4336e133a58043d4178c7248aafafecc750d0a79
Ashish Karki <akarki@mimecast.com>
2018-05-13 17:35:52 -0400

    Using streams instead of a full buffer when responding.

--- nodeJs_WebServer_Section7/app.js
+++ nodeJs_WebServer_Section7/app.js
@@ -2,8 +2,11 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  const html = fs.readFileSync(__dirname + '/index.html');
-  res.end(html);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  // let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+  // const message = "Hello from JS code....";
+  // html = html.replace('{Message}', message);
+  // res.end(html);
+  fs.createReadStream(__dirname + '/index.html').pipe(res);
 }).listen(1337, '127.0.0.1');
 
