b369ab4f6360ef4cf69b46ba2d23d26f94f77d8e
Ashish Karki <akarki@mimecast.com>
2018-05-14 20:11:06 -0400

    Using express with template engine to render pages and also using middle ware to route requests.

--- nodeJs_with_express/app.js
+++ nodeJs_with_express/app.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', (req, res, next) => {
+  console.log('Request Url: ', req.url);
+  next();
+});
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('<html><body><h1>Hello from Express</h1></body></html>')
+  // res.send('<html><head><link href="assets/style.css" type="text/css" rel="stylesheet"/></head><body><h1>Hello from Express</h1></body></html>')
+  res.render('index');
 });
 
 app.get('/person/:id', (req, res) => {
-  res.send('<html><body><h1>Person:'+ req.params.id +'</h1></body></html>')
+  // res.send('<html><body><h1>Person:'+ req.params.id +'</h1></body></html>')
+  res.render('person', { ID: req.params.id, Qstr: req.query.qstr })
 });
 
 app.get('/api', (req, res) => {
